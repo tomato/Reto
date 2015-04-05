fdfd3ef0094311d188deb9ec99d5d647394b5d16
Tom Howlett <tom@tomhowlett.com>
2015-04-05 15:29:13 +0000

    initial setup in c9

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: 'mongodb://localhost/mean-dev',
+		uri: 'mongodb://localhost/reto',
 		options: {
 			user: '',
 			pass: ''
@@ -18,7 +18,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Development Environment'
+		title: 'Reto - creating those redirects'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
